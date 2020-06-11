0e04eb459d981ba2b50ce2cf39c8bca7e24f4490
Ajay Mourya <ajaymourya8553@gmail.com>
2020-06-11 22:58:25 +0530

    adjacency list representation of graph

---
+++ src/com/ajaymourya/AdjListGraph.java
@@ -0,0 +1,36 @@
+package com.ajaymourya;
+
+import java.util.ArrayList;
+
+public class AdjListGraph {
+
+    static void addEdge(ArrayList<ArrayList<Integer>> graph, int s, int d) {
+        graph.get(s).add(d);
+        graph.get(d).add(s);
+    }
+
+    // Print the graph
+    static void printGraph(ArrayList<ArrayList<Integer>> graph) {
+        for (int i = 0; i < graph.size(); i++) {
+            System.out.println("\nVertex " + i + ":");
+            for (int j = 0; j < graph.get(i).size(); j++) {
+                System.out.print(" -> " + graph.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 5; i++)
+            graph.add(new ArrayList<Integer>());
+
+        // Add edges
+        addEdge(graph, 0, 1);
+        addEdge(graph, 0, 2);
+        addEdge(graph, 0, 3);
+        addEdge(graph, 1, 2);
+
+        printGraph(graph);
+    }
+}
