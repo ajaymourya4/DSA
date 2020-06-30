af125e9bbd9b7673e5bce414835157452a1c4eae
Ajay Mourya <ajaymourya8553@gmail.com>
2020-06-30 06:57:53 +0530

    selection sort added

---
+++ src/com/ajaymourya/sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.ajaymourya.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public void selectionSort(int[] array) {
+        int size = array.length;
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (array[j] < array[minIndex])
+                    minIndex = j;
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {-2, 45, 0, 11, -9};
+        BubbleSort sort = new BubbleSort();
+        sort.bubbleSort(data);
+        System.out.println("Sorted list");
+        System.out.println(Arrays.toString(data));
+    }
+}
