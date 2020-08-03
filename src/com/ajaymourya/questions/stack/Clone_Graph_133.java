59092b0ab92ba2bfb6a0499b50eae05eb71ffbb8
Ajay Mourya <ajaymourya8553@gmail.com>
2020-08-03 21:55:40 +0530

    clone graph

---
+++ src/com/ajaymourya/questions/stack/Clone_Graph_133.java
@@ -0,0 +1,45 @@
+package com.ajaymourya.questions.stack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+class CloneGraph {
+    public HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+
+    public Node cloneGraph(Node node) {
+        return clone(node);
+    }
+
+    public Node clone(Node node) {
+        if (node == null)
+            return node;
+        if (map.containsKey(node.val))
+            return map.get(node.val);
+        Node newNode = new Node(node.val, new ArrayList<Node>());
+        map.put(newNode.val, newNode);
+        for (Node neighbor : node.neighbors)
+            newNode.neighbors.add(clone(neighbor));
+        return newNode;
+    }
+}
