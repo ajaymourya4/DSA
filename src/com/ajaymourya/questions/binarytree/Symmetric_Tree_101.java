9fcac401bf08c6369a65b99fc2389733c7b82546
Ajay Mourya <ajaymourya8553@gmail.com>
2020-08-15 14:48:20 +0530

    symmetric tree

---
+++ src/com/ajaymourya/questions/binarytree/Symmetric_Tree_101.java
@@ -0,0 +1,58 @@
+package com.ajaymourya.questions.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ *
+ * But the following [1,2,2,null,3,null,3] is not:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ */
+class SymmetricTreeRecursive {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val) && isMirror(t1.right, t2.left) && isMirror(t1.left, t2.right);
+    }
+}
+
+class SymmetricTreeIterative {
+    public boolean isSymmetric(TreeNode root) {
+        //we can also use stack
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            queue.add(t1.right);
+            queue.add(t2.left);
+            queue.add(t1.left);
+            queue.add(t2.right);
+        }
+        return true;
+    }
+}
