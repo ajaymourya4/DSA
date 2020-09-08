d23312a9d6a31bc041bb85b6e8183c906745eb15
Ajay Mourya <ajaymourya8553@gmail.com>
2020-09-08 13:57:06 +0530

    remove element

---
+++ src/com/ajaymourya/questions/arrays/Remove_Element.java
@@ -0,0 +1,14 @@
+package com.ajaymourya.questions.arrays;
+
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
