fa7720fbc0b69f9799cc979340f69d2b5943f8f4
Ajay Mourya <ajaymourya8553@gmail.com>
2020-09-08 07:55:06 +0530

    longest common prefix

---
+++ src/com/ajaymourya/questions/arrays/Longest_Common_Prefix_14.java
@@ -0,0 +1,24 @@
+package com.ajaymourya.questions.arrays;
+
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        String longestCommonPrefix = "";
+        if (strs == null || strs.length == 0) {
+            return longestCommonPrefix;
+        }
+
+        int index = 0;
+        for (char c : strs[0].toCharArray()) {
+            for (int i = 1; i < strs.length; i++) {
+                if (index >= strs[i].length() || strs[i].charAt(index) != c) {
+                    return longestCommonPrefix;
+                }
+            }
+
+            longestCommonPrefix += c;
+            index++;
+        }
+
+        return longestCommonPrefix;
+    }
+}
