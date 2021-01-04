f8cfe874611ba6b1a5399c4b9fad1cabb3ca93b2
ajaymourya4 <ajaymourya8553@gmail.com>
2021-01-04 11:28:22 +0530

    question added

---
+++ src/com/ajaymourya/patterns/arrays/Contains_Duplicate_217.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for (int num : nums) {
+            if (hashSet.contains(num)) {
+                return true;
+            }
+            hashSet.add(num);
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
