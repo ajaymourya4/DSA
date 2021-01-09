166a7459d4f85fe0fcac37ea916c51a9f3f60b25
ajaymourya4 <ajaymourya8553@gmail.com>
2021-01-09 17:20:24 +0530

    spiral matrix

---
+++ src/com/ajaymourya/patterns/arrays/54_Spiral_Matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (matrix.length == 0) {
+            return list;
+        }
+        int rowBegin = 0, rowEnd = matrix.length-1, colBegin = 0, colEnd = matrix[0].length-1;
+        while(rowBegin <= rowEnd && colBegin <= colEnd){
+            for(int i=colBegin; i<=colEnd; i++){
+                list.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+            for(int i=rowBegin; i<=rowEnd; i++){
+                list.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+            if(rowBegin <= rowEnd){
+                for(int i=colEnd; i>=colBegin; i--){
+                    list.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+            if(colBegin <= colEnd){
+                for(int i=rowEnd; i>=rowBegin; i--){
+                    list.add(matrix[i][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+        return list;
+    }
+}
